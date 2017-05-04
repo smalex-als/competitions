37256f331b188f34c7199619524292520ab2524f
Alexey Smychagin <smalex69@gmail.com>
2017-05-04 17:53:52 -0400

    add P805

---
+++ codeforces/P805/B/B.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func solve() {
+	n := readInt()
+	a := make([]byte, n)
+	for i := 0; i < n; i++ {
+		a[i] = byte('a' + ((i / 2) % 2))
+	}
+	fmt.Println(string(a))
+}
+
+var scanner *bufio.Scanner
+
+func main() {
+	scanner = bufio.NewScanner(os.Stdin)
+	const MaxTokenLength = 1024 * 1024
+	scanner.Buffer(make([]byte, 0, MaxTokenLength), MaxTokenLength)
+	scanner.Split(bufio.ScanWords)
+	solve()
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// IO
+
+func readString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func readInt() int {
+	val, _ := strconv.Atoi(readString())
+	return val
+}
+
+func readInt64() int64 {
+	v, _ := strconv.ParseInt(readString(), 10, 64)
+	return v
+}
+
+func readIntArray(sz int) []int {
+	a := make([]int, sz)
+	for i := 0; i < sz; i++ {
+		a[i] = readInt()
+	}
+	return a
+}
+
+func readInt64Array(n int) []int64 {
+	res := make([]int64, n)
+	for i := 0; i < n; i++ {
+		res[i] = readInt64()
+	}
+	return res
+}
